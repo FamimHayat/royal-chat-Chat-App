bb2765ba282326b7b1b912e9e1bc8589f5b23cee
famim2002 <famimhayatyash@gmail.com>
2025-07-09 03:57:30 +0600

    myProfile

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
-import { MdOutlineModeEditOutline } from "react-icons/md";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { getAuth, updateProfile } from "firebase/auth";
+import { signedUser } from "../../reduxStorage/slices/authSlice";
 
 const MyProfile = () => {
+  const auth = getAuth();
+  const myData = useSelector((state) => state.signedUserData.UserData);
   const [isEditing, setIsEditing] = useState(false);
-  const [name, setName] = useState("Famim Hayat");
-  const [tempName, setTempName] = useState(name);
+  const [name, setName] = useState("");
+
+  const dispatch = useDispatch();
 
   const handleUpdate = () => {
-    setName(tempName);
     setIsEditing(false);
+
+    updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: "https://example.com/jane-q-user/profile.jpg",
+    })
+      .then(() => {
+        dispatch(signedUser(auth.currentUser));
+
+        setIsEditing(false);
+      })
+      .catch((error) => {});
   };
 
   return (
@@ -23,7 +40,6 @@ const MyProfile = () => {
       </style>
 
       <div className="md:scale-120 lg:scale-150 xl:scale-180 relative w-[240px] h-[330px] mx-auto mt-[250px] rounded-2xl overflow-hidden shadow-[0px_8px_28px_-9px_rgba(0,0,0,0.45)] bg-transparent">
-        {/* Animated Background Waves */}
         <div
           className="absolute w-[540px] h-[700px] opacity-60 top-0 left-0 -ml-1/2 -mt-[70%] bg-gradient-to-tr from-[#af40ff] via-[#5b42f3] to-[#00ddeb] rounded-[40%]"
           style={{ animation: "wave 25s linear infinite" }}
@@ -37,21 +53,17 @@ const MyProfile = () => {
           style={{ animation: "wave 15s linear infinite" }}
         />
 
-        {/* Content */}
         <div className="absolute top-[1.6em] left-0 right-0">
-          {/* Profile Picture */}
           <div className=" w-30  md:w-35 lg:w-40 mx-auto rounded-full mb-5 border-5 border-transparent outline-4 outline-white">
             <img src="/my-image.jpg" alt="Profile" className="rounded-full" />
           </div>
 
-          {/* Name and Actions */}
           <div className="flex flex-col justify-center items-center gap-3">
             {isEditing ? (
               <>
                 <input
                   type="text"
-                  value={tempName}
-                  onChange={(e) => setTempName(e.target.value)}
+                  onChange={(e) => setName(e.target.value)}
                   className="text-center px-3 py-1 rounded bg-[#ffffff10] text-white text-[17px] outline-none border border-white/20"
                 />
                 <button
@@ -64,7 +76,7 @@ const MyProfile = () => {
             ) : (
               <>
                 <p className="w-full text-white text-center font-semibold text-[20px] p-1 bg-[#00000070]">
-                  {name}
+                  {myData.displayName}
                 </p>
                 <button
                   className="text-white text-sm px-3 py-1 bg-[#00000070] rounded hover:bg-[#ffffff20] transition cursor-pointer hover:underline"
@@ -76,7 +88,6 @@ const MyProfile = () => {
             )}
           </div>
 
-          {/* Static text */}
           <div className="text-[14px] text-white text-center font-light mt-4 lowercase underline">
             my account
           </div>
