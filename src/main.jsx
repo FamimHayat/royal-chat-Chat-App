6c45f9439950ac1900ae0b0afb8602066a4f86f1
famim2002 <famimhayatyash@gmail.com>
2025-07-19 17:22:38 +0600

    error.fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+i
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -7,9 +7,9 @@ import { Provider } from "react-redux";
  import store  from "../reduxStorage/Store.js";
 
 createRoot(document.getElementById("root")).render(
-   <React.StrictMode>
+ 
     <Provider store={store} >
       <App />
     </Provider>
-   </React.StrictMode>
+   
 );
