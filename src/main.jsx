c065aa87f9ef3f1e277f14783bd742a3a619eb48
famim2002 <famimhayatyash@gmail.com>
2025-07-19 17:20:43 +0600

    error fix main.js

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { app } from "../fireBaseConfig.js";
 import { Provider } from "react-redux";
-import store from "../reduxStorage/store.js"
+ import store  from "../reduxStorage/Store.js";
 
 createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
+   <React.StrictMode>
     <Provider store={store} >
       <App />
     </Provider>
-  // </React.StrictMode>
+   </React.StrictMode>
 );
